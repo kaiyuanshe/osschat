eeade4d9831ab8748e38044a2307edf9b6ecf057
Huan LI <zixia@zixia.net>
2019-11-30 20:08:41 +0800

    add Wechaty Starter module

---
+++ src/start-bot.ts
@@ -0,0 +1,23 @@
+import {
+  Wechaty,
+}                   from 'wechaty'
+
+import {
+  log,
+}               from './config'
+
+export async function startBot (wechaty: Wechaty): Promise<void> {
+  log.verbose('startBot', 'startBot(%s)', wechaty)
+
+  wechaty
+    .on('scan',         './handlers/on-scan')
+    .on('error',        './handlers/on-error')
+    .on('friendship',   './handlers/on-friendship')
+    .on('logout',       './handlers/on-logout')
+    .on('login',        './handlers/on-login')
+    .on('message',      './handlers/on-message')
+    .on('room-topic',   './handlers/on-room-topic')
+    .on('room-invite',  './handlers/on-room-invite')
+    .on('room-join',    './handlers/on-room-join')
+    .on('room-leave',   './handlers/on-room-leave')
+}

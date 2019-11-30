cdd48198a8e35948f15e54886c86e17b99fa5186
Huan LI <zixia@zixia.net>
2019-11-30 20:08:12 +0800

    add Configuration setting file

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+/**
+ * VERSION
+ */
+import readPkgUp from 'read-pkg-up'
+
+export {
+  log,
+}               from 'brolog'
+
+const pkg = readPkgUp.sync({ cwd: __dirname }).pkg
+export const VERSION = pkg.version
+
+/**
+ * Env Vars
+ */
+export const PORT = process.env.PORT || 8788

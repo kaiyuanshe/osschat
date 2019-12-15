01741b5f63b336f65c77759e7999b217004f2322
lijiarui <ruiruibupt@126.com>
2019-12-15 16:49:41 +0800

    add room join demo #12

--- src/handlers/on-message.ts
+++ src/handlers/on-message.ts
@@ -1,6 +1,7 @@
 import {
   log,
   Message,
+  Room,
   Wechaty,
 }             from 'wechaty'
 
@@ -8,5 +9,21 @@ export default async function onMessage (
   this    : Wechaty,
   message : Message,
 ): Promise<void> {
+  const text = message.text()
+  const contact = message.from()
+  if (!contact) {
+    return
+  }
+  if (text === 'oss') {
+
+    // To Be Fix: Change "OSS Bot ChatOps" Group Name to actual group name
+    const room = await Room.find({ topic: 'OSS Bot ChatOps' })
+    if (room) {
+      await room.add(contact)
+
+      // To Be Fix: Change a formal welcome message
+      await room.say('Welcome to join OSS Bot ChatOps Group', contact)
+    }
+  }
   log.info('on-message', 'onMessage(%s)', message)
 }

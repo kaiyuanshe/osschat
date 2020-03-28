c94307b2d3489a29c2d1349f5d02baafb2e1d107
dailidong <dailidong66@gmail.com>
2020-03-28 22:46:44 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -42,10 +42,10 @@ export interface RepoConfig {
 export const managedRepoConfig: RepoConfig = {
   'BUPT/ai-ml.club': '18968477245@chatroom',
   'DSExtension/DSExtension': '18039997009@chatroom',
-  'apache/incubator-dolphinscheduler' : [
+  'apache/incubator-dolphinscheduler'     : [
     '8676247154@chatroom',
     '19237597168@chatroom',
-    '19380384367@chatroom'
+    '19380384367@chatroom',
   ], 
   'apache/incubator-doris' : '18440801276@chatroom',
   'apache/incubator-iotdb' : '18378203056@chatroom',

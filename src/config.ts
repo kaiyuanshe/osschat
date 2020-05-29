cacd86e431868da4ce04d8c193bc8d3e2191f853
dailidong <dailidong66@gmail.com>
2020-05-29 16:56:06 +0800

    add groups for DolphinScheduler

--- src/config.ts
+++ src/config.ts
@@ -51,6 +51,8 @@ export const managedRepoConfig: RepoConfig = {
     '8676247154@chatroom',
     '19237597168@chatroom',
     '19380384367@chatroom',
+    '19146409029@chatroom',
+    '20259685174@chatroom',
   ],
   'apache/incubator-doris' : '18440801276@chatroom',
   'apache/incubator-iotdb' : '18378203056@chatroom',

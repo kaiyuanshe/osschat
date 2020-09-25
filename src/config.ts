8c3dddd5e6a4eb74fae1b72ea2a7a1caabf0ebba
johnnywang <wpeace1212@gmail.com>
2020-09-25 19:52:17 +0800

    add osschat to wedatasphere group

--- src/config.ts
+++ src/config.ts
@@ -67,5 +67,19 @@ export const managedRepoConfig: RepoConfig = {
     '4808709382@chatroom',
     '4344886880@chatroom',
   ],
+ 'WeBankFinTech/Linkis' : [
+   17539577298@chatroom,
+   18720976935@chatroom,
+   18815198079@chatroom,
+   20244382563@chatroom,
+   20071581452@chatroom,
+  ],
+'WeBankFinTech/DataSphereStudio' : [
+   17539577298@chatroom,
+   18720976935@chatroom,
+   18815198079@chatroom,
+   20244382563@chatroom,
+   20071581452@chatroom,
+  ],
   ...projectsRepoConfig,
 }

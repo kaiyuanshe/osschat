349f37924e1d3bfb0c2e895ff91681ad460dc369
Huan <zixia@zixia.net>
2021-01-05 16:45:08 +0800

    add zixia bbs room for zixia bbs repo

--- src/projects/zixia.ts
+++ src/projects/zixia.ts
@@ -9,7 +9,8 @@ import {
 }                 from '../config'
 
 export const config: RepoConfig = {
-  'zixia/aka.org.cn': '3408538832@chatroom',
+  'zixia/aka.org.cn'    : '3408538832@chatroom',
+  'zixia/bbs.zixia.net' : '5297010089@chatroom',
 }
 
 export default config

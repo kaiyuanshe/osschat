a01d4b78c3669cabb1b3541a21dc9e4714327611
Huan <zixia@zixia.net>
2021-05-29 15:01:27 +0800

    fix probot v11 port listen new requirement: promise returned by app must be resolved before listen port

--- bin/main.ts
+++ bin/main.ts
@@ -3,11 +3,11 @@ import {
   ApplicationFunctionOptions,
 }                             from 'probot' // eslint-disable-line no-unused-vars
 
-import { Command } from 'commander'
+// import { Command } from 'commander'
 
 import {
   log,
-  VERSION,
+  // VERSION,
 }                     from '../src/config'
 import { getBot }     from '../src/get-bot'
 import { setupBot }   from '../src/setup-bot'
@@ -16,23 +16,21 @@ import { setupFinis } from '../src/setup-finis'
 import {
   commentIssue,
   openIssue,
-}                 from '../src/issue-handlers'
-import { configureRoutes } from '../src/routers'
+}                           from '../src/issue-handlers'
+import { configureRoutes }  from '../src/routers'
 
 export = async (
   app: Probot,
-  {
-    getRouter,
-  }: ApplicationFunctionOptions,
+  options: ApplicationFunctionOptions,
 ) => {
-  if (!getRouter) {
+  if (!options.getRouter) {
     throw new Error('getRouter() is required for OSSChat')
   }
 
   app.on('issue_comment.created', commentIssue)
   app.on('issues.opened', openIssue)
 
-  configureRoutes(getRouter())
+  configureRoutes(options.getRouter())
 
   // For more information on building apps:
   // https://probot.github.io/docs/
@@ -40,15 +38,15 @@ export = async (
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 
-  const program = new Command()
-  program
-    .version(VERSION)
-    .option('-d, --debug', 'enable debug mode')
+  // const program = new Command()
+  // program
+  //   .version(VERSION)
+  //   .option('-d, --debug', 'enable debug mode')
 
-  program.parse(process.argv)
-  if (program.opts().debug) {
-    process.env.DEBUG = program.opts().debug.toString()
-  }
+  // program.parse(process.argv)
+  // if (program.opts().debug) {
+  //   process.env.DEBUG = program.opts().debug.toString()
+  // }
 
   log.verbose('main', 'main()')
 
@@ -58,5 +56,13 @@ export = async (
   await setupBot()
   await setupFinis(bot)
 
-  await bot.state.ready('off')
+  /**
+   * Huan(202105):
+   *  if the returned promise from this function (app)
+   *  will not resolved, then the HTTP listener will not be started.
+   *
+   * Comment out the following code will resolve
+   *  the heroku R10 (boot timeout) problem
+   */
+  // await bot.state.ready('off')
 }

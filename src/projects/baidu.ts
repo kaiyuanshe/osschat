d3267168bf17f5cc2fe47d347b4dca4ccc84b35b
Huan <zixia@zixia.net>
2021-05-31 14:37:26 +0800

    use OSSChat repo

--- src/projects/baidu.ts
+++ src/projects/baidu.ts
@@ -1,6 +1,6 @@
 /**
- * Project: Wechaty
- * GitHub: https://github.com/wechaty/
+ * Project: OSSChat
+ * GitHub: https://github.com/kaiyuanshe/osschat
  * Maintainer: Huan LI (李卓桓) https://github.com/huan
  *
  */

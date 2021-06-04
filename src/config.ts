31a263cee9ce16244907ed647e3b26750a1b0dd6
Huan <zixia@zixia.net>
2021-06-04 11:12:08 +0800

    restore to use WeChat room instead of WeCom room

--- src/config.ts
+++ src/config.ts
@@ -19,19 +19,12 @@ const VERSION = pkg.version
  */
 // export const PORT = process.env.PORT || 8788
 
-const WECOM_CHATOPS_ROOM_ID   = '21453880635@chatroom'  // WeCom: ChatOps - OSSChat
-const WECHAT_CHATOPS_ROOM_ID  = '18995691396@chatroom'  // WeChat: ChatOps - OSSChat
+// const CHATOPS_ROOM_ID   = '21453880635@chatroom'  // WeCom: ChatOps - OSSChat
+const CHATOPS_ROOM_ID  = '18995691396@chatroom'  // WeChat: ChatOps - OSSChat
 
-const WECOM_HEARTBEAT_ROOM_ID   = '24980472405@chatroom'    // WeCom: ChatOps - Heartbat
-const WECHAT_HEARTBEAT_ROOM_ID  = '17376996519@chatroom'  // WeChat: ChatOps - Heartbeat
+// const HEARTBEAT_ROOM_ID   = '24980472405@chatroom'    // WeCom: ChatOps - Heartbat
+const HEARTBEAT_ROOM_ID  = '17376996519@chatroom'  // WeChat: ChatOps - Heartbeat
 
-void WECOM_CHATOPS_ROOM_ID
-void WECHAT_CHATOPS_ROOM_ID
-void WECOM_HEARTBEAT_ROOM_ID
-void WECHAT_HEARTBEAT_ROOM_ID
-
-const CHATOPS_ROOM_ID    = WECOM_CHATOPS_ROOM_ID
-const HEARTBEAT_ROOM_ID  = WECOM_HEARTBEAT_ROOM_ID
 const DEVELOPER_ROOM_ID  = '17591588552@chatroom'  // WeChat: OSSChat Project Group
 
 function debug () : boolean {

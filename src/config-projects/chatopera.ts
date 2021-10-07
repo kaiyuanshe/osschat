f66af88991d57ae635155308829f27abe67b7779
Hai Liang Wang <hain@chatopera.com>
2021-10-07 10:35:43 +0800

    https://github.com/kaiyuanshe/osschat/issues/153 add test profile

--- src/config-projects/chatopera.ts
+++ src/config-projects/chatopera.ts
@@ -9,8 +9,7 @@ import type { RepoConfig } from './config.js'
 export const config: RepoConfig = {
   'chatopera/cskefu': [
     /* Chatopera智能客服-客户群 */ '22888396746@chatroom',
-    /* Chatopera智能客服-客户群02 */ '25014086676@chatroom',
-    /* Chatopera测试群1 */ '20687119169@chatroom'],
+    /* Chatopera智能客服-客户群02 */ '25014086676@chatroom'],
   'chatopera/docs': [
     /* Chatopera智能客服-客户群 */ '22888396746@chatroom',
     /* Chatopera智能客服-客户群02 */ '25014086676@chatroom'],

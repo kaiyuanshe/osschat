2abd3fdfd050d51aa29198655cb7fc91e3ff2647
Hai Liang Wang <hain@chatopera.com>
2021-10-09 11:29:50 +0800

    Add demo repo and upgrade chatopera plugin for mapping bot per projectadd test profile (#171)
    * https://github.com/kaiyuanshe/osschat/issues/153 add test profile
    
    * https://github.com/kaiyuanshe/osschat/issues/153 Add demo repo and upgrade chatopera plugin for mapping bot per project
    
    * https://github.com/kaiyuanshe/osschat/issues/153 Add demo repo and upgrade chatopera plugin for mapping bot per project
    
    * 0.10.34
    
    * 0.10.35
    
    * https://github.com/kaiyuanshe/osschat/issues/153 enable a single bot per github owner with star
    
    * 0.10.36

--- src/config-projects/chatopera.ts
+++ src/config-projects/chatopera.ts
@@ -7,11 +7,7 @@
 import type { RepoConfig } from './config.js'
 
 export const config: RepoConfig = {
-  'chatopera/cskefu': [
-    /* Chatopera智能客服-客户群 */ '22888396746@chatroom',
-    /* Chatopera智能客服-客户群02 */ '25014086676@chatroom',
-    /* Chatopera测试群1 */ '20687119169@chatroom'],
-  'chatopera/docs': [
+  'chatopera/*': [
     /* Chatopera智能客服-客户群 */ '22888396746@chatroom',
     /* Chatopera智能客服-客户群02 */ '25014086676@chatroom'],
 }
---
+++ src/config-projects/hailiang-wang.ts
@@ -0,0 +1,14 @@
+/**
+ * Project: OSSChat
+ * GitHub: https://github.com/kaiyuanshe/osschat
+ * Maintainer: Chatopera https://github.com/hailiang-wang
+ *
+ */
+import type { RepoConfig } from './config.js'
+
+export const config: RepoConfig = {
+  'hailiang-wang/issues': [
+    /* Chatopera测试群1 */ '20687119169@chatroom'],
+}
+
+export default config

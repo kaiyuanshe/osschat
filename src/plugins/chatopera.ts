ef1fab17540065411d2c4d44d45252284c543250
mukaiu <mukaiu@live.cn>
2021-10-23 01:23:48 +0800

    https://github.com/kaiyuanshe/osschat/issues/175 import Bot FAQ on starting

--- src/plugins/chatopera.ts
+++ src/plugins/chatopera.ts
@@ -9,6 +9,8 @@ import {
   projectsRepoConfig,
 }                       from '../config-projects/mod.js'
 
+import path             from 'path'
+
 let plugin: undefined | WechatyPlugin
 
 // Auto load Wechaty Chatopera Plugin with ENV variables
@@ -19,6 +21,7 @@ if (
   plugin = WechatyChatopera({
     clientId: process.env['CHATOPERA_DEFAULT_CLIENTID'],
     faqBestReplyThreshold: process.env['CHATOPERA_FAQ_BESTREPLY_THRES'] ? parseFloat(process.env['CHATOPERA_FAQ_BESTREPLY_THRES']) : undefined,
+    faqPath: path.join(__dirname, '../../faqs'),
     faqSuggReplyThreshold: process.env['CHATOPERA_FAQ_SUGGREPLY_THRES'] ? parseFloat(process.env['CHATOPERA_FAQ_SUGGREPLY_THRES']) : undefined,
     mention: false,
     personalAccessToken: process.env['CHATOPERA_PERSONAL_ACC_TOKEN'],

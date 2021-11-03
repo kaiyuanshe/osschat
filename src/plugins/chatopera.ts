3e0e74f9bfa0dce8391dc4ede8f597bfeb7cf4af
mukaiu <mukaiu@live.cn>
2021-11-03 16:05:22 +0800

    Fix FAQ path

--- src/plugins/chatopera.ts
+++ src/plugins/chatopera.ts
@@ -9,10 +9,6 @@ import {
   projectsRepoConfig,
 }                       from '../config-projects/mod.js'
 
-import {
-  fileURLToPath,
-}                       from 'url'
-
 import path             from 'path'
 
 let plugin: undefined | WechatyPlugin
@@ -22,13 +18,10 @@ if (
   (process.env['CHATOPERA_DEFAULT_CLIENTID'] && process.env['CHATOPERA_DEFAULT_SECRET'])
   || process.env['CHATOPERA_PERSONAL_ACC_TOKEN']
 ) {
-
-  const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
   plugin = WechatyChatopera({
     clientId: process.env['CHATOPERA_DEFAULT_CLIENTID'],
     faqBestReplyThreshold: process.env['CHATOPERA_FAQ_BESTREPLY_THRES'] ? parseFloat(process.env['CHATOPERA_FAQ_BESTREPLY_THRES']) : undefined,
-    faqPath: path.join(__dirname, '../../../faqs'),
+    faqPath: path.resolve('./faqs'),
     faqSuggReplyThreshold: process.env['CHATOPERA_FAQ_SUGGREPLY_THRES'] ? parseFloat(process.env['CHATOPERA_FAQ_SUGGREPLY_THRES']) : undefined,
     mention: false,
     personalAccessToken: process.env['CHATOPERA_PERSONAL_ACC_TOKEN'],

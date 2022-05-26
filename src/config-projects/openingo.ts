57fc1fd2baa85d313252d7621b033cb146054aa7
Qicz <zcq@zhucongqi.cn>
2022-05-26 12:00:18 +0800

    add openingo config. (#211)
    * add openingo config.
    
    * reset sort

---
+++ src/config-projects/openingo.ts
@@ -0,0 +1,17 @@
+/**
+ * Project: OSSChat
+ * GitHub: https://github.com/kaiyuanshe/osschat
+ * Maintainer: Huan LI (李卓桓) https://github.com/huan
+ *
+ */
+import type {
+  RepoConfig,
+}                 from './config.js'
+
+export const config: RepoConfig = {
+  'openingo/.github': '20527830074@chatroom',
+  'openingo/envcd': '20527830074@chatroom',
+  'openingo/godkits': '20527830074@chatroom',
+}
+
+export default config
